56b83b2d26bfc84cde540789a87ed504044545d4
alexb <alexb@AlexSurface>
2016-10-05 00:03:49 +0200

    Completata specifica R2

--- 20 Aeroporto/src/Aerei/Compagnia.java
+++ 20 Aeroporto/src/Aerei/Compagnia.java
@@ -1,18 +1,35 @@
 package Aerei;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 
 public class Compagnia {
+	
+	private String nome;
+	private AeroportiMondo aeroportiCompagnia;
+	private Map<String, Integer> aerei = new TreeMap<String, Integer>();
 
 	public Compagnia(String nome, AeroportiMondo am){
+	
+		this.nome = nome;
+		aeroportiCompagnia = am;
 	}
 	
 	public void addAereo(String cod, int pass) throws InvalidCode {
+		
+		if (!aerei.containsKey(cod)){
+			aerei.put(cod, pass);
+			return;
+		}
+		throw new InvalidCode();
 	}
 	
 	public List<String> getAerei(){
-		return null;
+		
+		return new ArrayList<String>(aerei.keySet());
 	}
 	
 	public void addVolo(String cod, String aer, String part, String arr, String giorno) throws InvalidCode {
